9956934797442effb47396c4463e4bf7f32080b7
tomato-tom <hajimeee49@gmail.com>
2026-01-22 16:22:15 +0900

    Add featur: 読み上げ

--- flash_card.sh
+++ flash_card.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 # 英単語フラッシュカード
 
+SPEECH=true  # 読み上げスイッチ true/false
+
 # 単語帳ファイル（CSV形式: 英単語,日本語訳）
 VOCAB_FILE="card/vocab.txt"
-
-# 単語帳ファイルがなければ作成
 if [ ! -f "$VOCAB_FILE" ] || [ ! -s $VOCAB_FILE ]; then
-    echo "apple,りんご" >> "$VOCAB_FILE"
-    echo "book,本" >> "$VOCAB_FILE"
-    echo "cat,猫" >> "$VOCAB_FILE"
-    echo "dog,犬" >> "$VOCAB_FILE"
-    echo "water,水" >> "$VOCAB_FILE"
+    exit 1
 fi
+# 後ほどjsonにするか
+
+# 英語読み上げ
+speech() {
+    local text="$@"
+    local tmp="/tmp/say.mp3"
+    local speed="${SPEED:-1.0}"    # 環境変数で速度指定
+
+    gtts-cli "$text" --output "$tmp"
+    ffplay -autoexit -nodisp -loglevel quiet -af "atempo=$speed" "$tmp"
+
+    [ -f "$tmp" ] && rm "$tmp"
+}
+
 
 tput civis
 echo "    === Flash Card ==="
@@ -38,12 +48,28 @@ while true; do
     english=$(echo "$WORD" | cut -d',' -f1)
     english_line=3
     english_color="\033[38;5;87m"
+    $SPEECH && speech "$english" &
 
     write_at $english_line center "$english" "$english_color"
     tput rc
     tput ed
 
-    echo "Do you know？"
+    echo "Any key to show answer"
+    read -n 1 -t 3 -s input
+
+    if [ "$input" = "q" ]; then
+        echo
+        break
+    fi
+
+    japanese=$(echo "$WORD" | cut -d',' -f2)
+    japanese_line=6
+    japanese_color="\033[0;32m"
+    
+    write_at $japanese_line center "$japanese" "$japanese_color"
+    tput rc
+    tput ed
+
     echo "e - Eesy"
     echo "m - Medium"
     echo "h - Hard"
@@ -62,14 +88,6 @@ while true; do
     else
         ((easy ++))
     fi
-    
-    japanese=$(echo "$WORD" | cut -d',' -f2)
-    japanese_line=6
-    japanese_color="\033[0;32m"
-    
-    write_at $japanese_line center "$japanese" "$japanese_color"
-
-    sleep 0.8
 
     # 文字をスペースで上書きでクリア
     count="$(echo $english | wc -c)"
@@ -79,6 +97,8 @@ while true; do
     count="$(echo $japanese | wc -c)"
     printf -v spaces "%*s" $count ""
     write_at $japanese_line center "$spaces"
+
+    sleep 0.7
 done
 
 tput rc
